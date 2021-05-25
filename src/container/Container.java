cb8ad6db5dc0fdc8c3b216e4c29ef63a84c5002d
gwanyong12 <pky4321@naver.com>
2021-05-25 22:43:04 +0900

    컨트롤러 구조 도입

--- src/container/Container.java
+++ src/container/Container.java
@@ -1,5 +1,29 @@
 package container;
 
-public class Container {
+import com.sbs.example.jspCommunity.controller.usr.ArticleController;
+import com.sbs.example.jspCommunity.controller.usr.MemberController;
+import com.sbs.example.jspCommunity.dao.ArticleDao;
+import com.sbs.example.jspCommunity.dao.MemberDao;
+import com.sbs.example.jspCommunity.service.ArticleService;
+import com.sbs.example.jspCommunity.service.MemberService;
 
+public class Container {
+	public static ArticleService articleService;
+	public static ArticleDao articleDao;
+	public static ArticleController articleController;
+
+	public static MemberService memberService;
+	public static MemberDao memberDao;
+	public static MemberController memberController;
+
+	static {
+		memberDao = new MemberDao();
+		articleDao = new ArticleDao();
+
+		memberService = new MemberService();
+		articleService = new ArticleService();
+
+		memberController = new MemberController();
+		articleController = new ArticleController();
+	}
 }
